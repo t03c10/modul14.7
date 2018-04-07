c2605694cea4ec3419abbc124841a8477f676a02
pawelz <t03c10@gmail.com>
2018-04-07 16:09:58 +0200

    initial commit

---
+++ components/Contacts.js
@@ -0,0 +1,26 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {className: 'app'},
+        React.createElement(ContactForm, {contact: contactForm}),
+        React.createElement(Contacts, {items: contacts}, {})
+      )
+    );
+  }
+});
+  
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
+    return (
+      React.createElement('ul', {className: 'contactsList'}, contacts)
+    );
+  }
+});
