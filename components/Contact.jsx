7885706750964dd5f5b1f7b917022b514d43ebc6
pawelz <t03c10@gmail.com>
2018-04-13 11:20:22 +0200

    Contact.js and ContactForm.js file extension changed to *.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,10 +3,10 @@ var Contact = React.createClass({
     item: React.PropTypes.object.isRequired
   },
 
-  render: function(){
-    return(
+  render: function () {
+    return (
       <div className={'contactItem'}>
-        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} />      
+        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} />
         <p className={'contactLabel'}> Imię: {this.props.item.firstName}
         </p>
         <p className={'contactLabel'}> Nazwisko: {this.props.item.lastName}
